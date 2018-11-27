56803975fa915a3cc65ace9685168df5e89cdd31
tlkmay0506 <tlkmay0506@gmail.com>
2018-11-27 00:25:41 -0600

    more

--- cachesim.py
+++ cachesim.py
@@ -1,8 +1,16 @@
 import sys
-
+import numpy as np
+import pandas as pd
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
+
+columns = ['Tag','Validbit', 'data', 'timestamp']
+df= pd.DataFrame(columns=columns)
+print(df.columns)
+
+
 #def Access(RW,addr):
+
 
     
