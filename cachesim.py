7981c781586c463186920c63f30db3b78de8b67a
Ty <tlkmay0506@gmail.com>
2018-11-27 22:02:19 -0600

    commit

--- cachesim.py
+++ cachesim.py
@@ -1,16 +1,21 @@
 import sys
-import numpy as np
-import pandas as pd
+
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 
-columns = ['Tag','Validbit', 'data', 'timestamp']
-
-cachedf= pd.DataFrame(columns=columns)
-print(df.columns)
+CacheDict = {'Tag': 0, 'Valid': 0, 'Data': 0}
 
 def Access(RW,addr):
+    CacheDict = {'Tag': 0, 'Valid': 0, 'Data': 0}
+    L=[]
+    if addr in CacheDict.keys():
+        L[0] = addr
+    elif  addr not in CacheDict.keys():
+         L.pop()
+
+
+
 #Check if in cache
 #cut out the bits( determine setID)
 #search set determine if addr is in cache, tag comparison
